8c8543b2750af8c04486a45e19ed17e93c652e34
Alishba Ansari <alishbaansari180@gmail.com>
2025-02-27 03:34:44 +0500

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # streamlit 
 import streamlit as st 
 
-st.set_page_config(page_title= "Growth-mindset-project", project_icon=" ⭐")
+st.set_page_config(page_title= "Growth-mindset-project", page_icon=" ⭐")
 st.title("Growth Mindset Challenge: Web App with Streamlit")
 
 st.header("🚀 Welcome to your Growth journey!")
